192d8988b85cc5573b109d0b74be08f5efaab788
wxmaudio <wxm224@163.com>
2016-05-17 16:13:49 +0800

    Add production plugin and config of webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,62 @@
 var path = require('path');
 var webpack = require('webpack');
+var CleanPlugin = require('clean-webpack-plugin');
+var production = process.env.NODE_ENV === 'production';
+
+var plugins = [
+  new webpack.optimize.CommonsChunkPlugin({
+        name:'common',
+        //filename: 'commons-[chunkhash].js',
+        children:true,
+        minChunks:2
+       })
+];
+
+if(production){
+  plugins = plugins.concat([
+    //production plugins go here
+
+        // Cleanup the builds/ folder before
+        // compiling our final assets
+        new CleanPlugin('dist'),
+
+        // This plugin looks for similar chunks and files
+        // and merges them for better caching by the user
+        new webpack.optimize.DedupePlugin(),
+
+        // This plugins optimizes chunks and modules by
+        // how much they are used in your app
+        new webpack.optimize.OccurenceOrderPlugin(),
+
+        // This plugin prevents Webpack from creating chunks
+        // that would be too small to be worth loading separately
+        new webpack.optimize.MinChunkSizePlugin({
+            minChunkSize: 51200, // ~50kb
+        }),
+
+        // This plugin minifies all the Javascript code of the final bundle
+        new webpack.optimize.UglifyJsPlugin({
+            mangle:   true,
+            compress: {
+                warnings: false, // Suppress uglification warnings
+            },
+        }),
+
+        // This plugins defines various variables that we can set to false
+        // in production to avoid code related to them from being compiled
+        // in our final bundle
+        new webpack.DefinePlugin({
+            __SERVER__:      !production,
+            __DEVELOPMENT__: !production,
+            __DEVTOOLS__:    !production,
+            'process.env':   {
+                BABEL_ENV: JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
+ 
+
+    ]);
+}
 
 var config = {
     //context:__dirname,
@@ -13,20 +70,13 @@ var config = {
     output: {
         path: path.join(__dirname,'dist'),
         publicPath: '/dist/',
-        filename: "[name].js",
-        chunkFilename: "[id].chunk.js"
+        filename: production ? '[name]-[hash].js':'[name].js',
+        chunkFilename: "[name]-[chunkhash]-chunk.js"
     },
 
-    plugins:[
-       new webpack.optimize.CommonsChunkPlugin({
-        name:'common',
-        //filename:'commons-[chunkhash].js'
-        children:true,
-        minChunks:2
-       })
-    ],
+    plugins:plugins,
     //publicPath: path.join(__dirname,'/dist'),
-    debug: true,
+    
     module: {
         loaders: [
           { 
@@ -47,8 +97,8 @@ var config = {
           }
         ]
     },
-    debug: true,
-    devtool: 'source-map',
+    debug: !production,
+    devtool: production ? false : 'eval',
     resolve: {
     // 现在可以写 require('file') 代替 require('file.js')
     extensions: ['', '.js', '.json'] 
